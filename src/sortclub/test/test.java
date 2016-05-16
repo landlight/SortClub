1b48cef6ad18bc324b6b02e5786c9354f682540c
landlight <ck.light@gmail.com>
2016-05-16 15:58:46 +0700

    updating bubblesort and testing

---
+++ src/sortclub/test/test.java
@@ -0,0 +1,15 @@
+package sortclub.test;
+
+import sortclub.sort.BubbleSort;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] a = {"eat","abce","abcd"};
+		BubbleSort b = new BubbleSort();
+		
+		b.stringSort(a);
+	}
+
+}
